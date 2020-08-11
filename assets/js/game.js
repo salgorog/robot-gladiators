5a450e56975adbf0bdaa6f02939b8e3ae63b0498
Aztec <salgorog@hotmail.com>
2020-08-11 17:10:00 -0600

    added health status to player and enemy

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,7 +24,21 @@ var fight = function() {
     // Log a resulting message to the console so we know that it worked.
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-      );
+    );
+    // check enemy's health
+    if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+    } 
+    else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    // check player's health
+    if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+     } 
+    else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
   };
 
 fight();
