c204ead66d95630552c2457d58c02e1c6f2dfdea
Aztec <salgorog@hotmail.com>
2020-08-13 03:19:07 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,14 +24,16 @@ var fight = function (enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + ' has decided to skip this fight. Goodbye!');
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney)
                 break;
             }
         }
 
-        // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
             playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
         );
@@ -50,7 +52,9 @@ var fight = function (enemyName) {
         }
 
         // remove players's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
             enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
         );
@@ -80,10 +84,9 @@ var startGame = function () {
 
             var pickedEnemyName = enemyNames[i];
 
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
 
             fight(pickedEnemyName);
-
             // if we're not at the last enemy in the array
             if (playerHealth > 0 && i < enemyNames.length - 1) {
 
@@ -94,17 +97,18 @@ var startGame = function () {
                 if (storeConfirm) {
                     shop();
                 }
-            }
-            else {
-                window.alert("You have lost your robot in battle! Game Over!");
-                break;
+
             }
         }
-        // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
-        endGame();
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
+    }
+    // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+    endGame();
 
-    };
-}
+};
 
 // function to end the entire game
 var endGame = function () {
@@ -173,6 +177,13 @@ var shop = function () {
             shop();
             break;
     }
+}
+
+// function to generate a random numeric value
+var randomNumber = function (min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
 };
 
 // start the game when the page loads
