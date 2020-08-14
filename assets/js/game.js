4af73555366e16a07df85e588a3b29e3b309fe58
Aztec <salgorog@hotmail.com>
2020-08-14 17:18:32 -0600

    added input 1, 2, and 3 to buy items at the shop

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,7 +20,7 @@ var fightOrSkip = function () {
             window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
             // subtract money from playerMoney for skipping, but don't let them go into the negative
             playerInfo.money = Math.max(0, playerInfo.money - 10);
-
+            console.log("your gold is", playerInfo.money)
             // return true if user wants to leave
             return true;
         }
@@ -143,21 +143,20 @@ var shop = function () {
 
     // ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
+
+    shopOptionPrompt = parseInt(shopOptionPrompt);
 
     // use switch to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL": // new case
-        case "refill":
+        case 1: // new case
             playerInfo.refillHealth();
             break;
-        case "UPGRADE": // new case
-        case "upgrade":
+        case 2: // new case
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE": // new case
-        case "leave":
+        case 3: // new case
             window.alert("Leaving the store.");
             break;
         default:
